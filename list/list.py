42f3a00956488a1fd8036fd88bcff5baf34aa8c1
Lydia Jenson <admin@Lydias-MacBook-Air.local>
2018-12-09 14:32:00 -0800

    added details of ec2 instance

--- list/list.py
+++ list/list.py
@@ -1,5 +1,13 @@
 import boto3
+import click
+
 session = boto3.Session(profile_name='default')
 ec2 = session.resource('ec2')
-for i in ec2.instances.all():
-    print(i)
+
+def list_instances():
+    for i in ec2.instances.all():
+        print(i)
+
+
+if __name__ == '__main__':
+    list_instances()
